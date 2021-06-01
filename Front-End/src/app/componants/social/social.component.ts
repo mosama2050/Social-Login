510066bd91852835ae9f5bda773812cb1ab36e9b
mosama2050 <37184677+mosama2050@users.noreply.github.com>
2021-06-01 17:42:11 +0200

    Show data of User (Facebook - Gmail)

--- Front-End/src/app/componants/social/social.component.ts
+++ Front-End/src/app/componants/social/social.component.ts
@@ -15,11 +15,19 @@ export class SocialComponent implements OnInit {
   ngOnInit(): void {
   }
   signInWithGoogle(): void {
-    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then(
+      data => {
+        console.log(data)
+      }
+    );
   }
 
   signInWithFB(): void {
-    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID).then(
+      data => {
+        console.log(data)
+      }
+    );
   }
 
 

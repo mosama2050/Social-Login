5beeba115a2734559cb8d8edb79a807c34305a50
mosama2050 <37184677+mosama2050@users.noreply.github.com>
2021-06-10 02:07:54 +0200

    jwt test and get token

--- Back-End/src/main/java/com/spring/social/config/SpringConfigue.java
+++ Back-End/src/main/java/com/spring/social/config/SpringConfigue.java
@@ -1,10 +1,15 @@
 package com.spring.social.config;
 
+import com.spring.social.config.jwt.JwtAuthorizationFilter;
+import com.spring.social.dao.UserRepository;
 import com.spring.social.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,18 +18,23 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@AllArgsConstructor
 public class SpringConfigue extends WebSecurityConfigurerAdapter {
 
-    private UserService userService;
+    private  final UserService userService;
+    private final UserRepository userRepository;
 
-    @Autowired
-    public SpringConfigue(UserService userService) {
-        this.userService = userService;
-    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authenticationProvider());
+    }
+
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Override
@@ -33,7 +43,10 @@ public class SpringConfigue extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(),userRepository))
                 .authorizeRequests()
+                .antMatchers("/auth/login")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
